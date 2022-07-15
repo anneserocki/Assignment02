fea920e6c92ecdca907568dbaaf2e56fee34a0c2
Your Name <anneserocki@gmail.comom>
2022-07-15 16:18:31 -0700

    finish4

---
+++ js/04-grade-assigner.js
@@ -0,0 +1,15 @@
+let score = prompt('Please enter a numbert within 1 to 100');
+
+if (score < 0 || score > 100) {
+    alert(`The number is not withn 1 to 100, please re-enter a number!`);
+} else if (score >= 90 ) {
+    console.log(`You received an A`);
+} else if (score >= 80 ) {
+    console.log(`You received a B`);    
+} else if (score >= 70) {
+    console.log(`You received a C`);    
+} else if (score >= 60) {
+    console.log(`You received a D`);
+} else if (score < 60 ) {
+    alert(`You received an F`);
+} // End if ...else if for grade
