35c495265d70d8a3e050b0dbb56d4a016337a78c
Your Name <anneserocki@gmail.comom>
2022-07-15 14:26:49 -0700

    finish3

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -4,6 +4,7 @@ let coinFlip = randomNum;
 
 console.log(coinFlip); 
 
+// Method 1: if...else if nested if ... else if 
 if (coinFlip <= 0) {
     document.write ('It\'s heads. ');
     if (choice === `heads`) {
@@ -11,7 +12,24 @@ if (coinFlip <= 0) {
     } else if (choice === `tails`) {
         alert('The flip was heads but you chose tails...you lose!');
     }; // End if...else for heads
-} ;
+}  else if (coinFlip >= 1 ) {
+    document.write ('It\'s tails. ');
+    if (choice ===`tails`) {
+        alert('The flip was tails and you chose tails...you win!');
+    } else if (choice === `heads`) {
+        alert('The flip was tails but you chose heads...you lose!');
+    }; //End if...else for tails
+} // End if for tails
+
+// Method 2: if nested if...else if
+if (coinFlip <= 0) {
+    document.write ('It\'s heads. ');
+    if (choice === `heads`) {
+        alert('The flip was heads and you chose heads...you win!');
+    } else if (choice === `tails`) {
+        alert('The flip was heads but you chose tails...you lose!');
+    }; // End if...else for heads
+}  // End if for heads
 if (coinFlip >= 1 ) {
     document.write ('It\'s tails. ');
     if (choice ===`tails`) {
@@ -19,4 +37,5 @@ if (coinFlip >= 1 ) {
     } else if (choice === `heads`) {
         alert('The flip was tails but you chose heads...you lose!');
     }; //End if...else for tails
-} // End if...else if
+} // End if for tails
+
